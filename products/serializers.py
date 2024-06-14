d74c2a8b2a56adaaef2ef00883007f04d21e33a2
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-06-14 08:25:02 -0700

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -22,7 +22,7 @@ class ProductListSerializer(TaggitSerializer,serializers.ModelSerializer):
     class Meta:
         model = Product
         # fields ='__all__'
-        fields =['name','brand','price','review_count', 'avg_rate','tags','image']
+        fields =['name','brand','price','review_count', 'avg_rate','tags','image','slug']
             
     
 class BrandDetailSerializer(serializers.ModelSerializer):
